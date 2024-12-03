55001d315931b98b3dc8da6533a065c907f3c46b
Sys_Br3ach3r <136063985+Horlalaycon@users.noreply.github.com>
2024-12-03 01:13:01 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const toggleButton = document.getElementById('toggle-theme');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
